692bf2af84cc76743d1ddc741e16daea3fe5ff3a
ahljljj <jliang0718@outlook.com>
2020-03-21 15:56:02 -0400

    Site updated: 2020-03-21 15:55:59

--- js/script.js
+++ js/script.js
@@ -1,88 +1,82 @@
-// declaraction of document.ready() function.
-(function () {
-    var ie = !!(window.attachEvent && !window.opera);
-    var wk = /webkit\/(\d+)/i.test(navigator.userAgent) && (RegExp.$1 < 525);
-    var fn = [];
-    var run = function () {
-        for (var i = 0; i < fn.length; i++) fn[i]();
-    };
-    var d = document;
-    d.ready = function (f) {
-        if (!ie && !wk && d.addEventListener)
-            return d.addEventListener('DOMContentLoaded', f, false);
-        if (fn.push(f) > 1) return;
-        if (ie)
-            (function () {
-                try {
-                    d.documentElement.doScroll('left');
-                    run();
-                } catch (err) {
-                    setTimeout(arguments.callee, 0);
-                }
-            })();
-        else if (wk)
-            var t = setInterval(function () {
-                if (/^(loaded|complete)$/.test(d.readyState))
-                    clearInterval(t), run();
-            }, 0);
-    };
-})();
+(function ($) {
+    $('.navbar-burger').click(function () {
+        $(this).toggleClass('is-active');
+        $('.navbar-main .navbar-start').toggleClass('is-active');
+        $('.navbar-main .navbar-end').toggleClass('is-active');
+    });
 
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.navbar-main').outerHeight();
 
-document.ready(
-    // toggleTheme function.
-    // this script shouldn't be changed.
-    function () {
-        var _Blog = window._Blog || {};
-        const currentTheme = window.localStorage && window.localStorage.getItem('theme');
-        const isDark = currentTheme === 'dark';
-        if (isDark) {
-            document.getElementById("switch_default").checked = true;
-            // mobile
-            document.getElementById("mobile-toggle-theme").innerText = "· Dark"
-        } else {
-            document.getElementById("switch_default").checked = false;
-            // mobile
-            document.getElementById("mobile-toggle-theme").innerText = "· Dark"
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
         }
-        _Blog.toggleTheme = function () {
-            if (isDark) {
-                document.getElementsByTagName('body')[0].classList.add('dark-theme');
-                // mobile
-                document.getElementById("mobile-toggle-theme").innerText = "· Dark"
-            } else {
-                document.getElementsByTagName('body')[0].classList.remove('dark-theme');
-                // mobile
-                document.getElementById("mobile-toggle-theme").innerText = "· Light"
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta) {
+            return;
+        }
+
+        // If they scrolled down and are past the navbar, add class .navbar-down.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight) {
+            var posY = Math.min(st, navbarHeight);
+            // Scroll Down
+            $('.navbar-main').css({
+                '-webkit-transform' : 'translateY(-' + posY + 'px)',
+                '-moz-transform'    : 'translateY(-' + posY + 'px)',
+                '-ms-transform'     : 'translateY(-' + posY + 'px)',
+                '-o-transform'      : 'translateY(-' + posY + 'px)',
+                'transform'         : 'translateY(-' + posY + 'px)'
+            });
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('.navbar-main').css({
+                    '-webkit-transform' : 'translateY(0px)',
+                    '-moz-transform'    : 'translateY(0px)',
+                    '-ms-transform'     : 'translateY(0px)',
+                    '-o-transform'      : 'translateY(0px)',
+                    'transform'         : 'translateY(0px)'
+                });
             }
-            document.getElementsByClassName('toggleBtn')[0].addEventListener('click', () => {
-                if (document.getElementsByTagName('body')[0].classList.contains('dark-theme')) {
-                    document.getElementsByTagName('body')[0].classList.remove('dark-theme');
-                } else {
-                    document.getElementsByTagName('body')[0].classList.add('dark-theme');
-                }
-                window.localStorage &&
-                window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light',)
-            })
-            // moblie
-            document.getElementById('mobile-toggle-theme').addEventListener('click', () => {
-                if (document.getElementsByTagName('body')[0].classList.contains('dark-theme')) {
-                    document.getElementsByTagName('body')[0].classList.remove('dark-theme');
-                    // mobile
-                    document.getElementById("mobile-toggle-theme").innerText = "· Light"
+        }
 
-                } else {
-                    document.getElementsByTagName('body')[0].classList.add('dark-theme');
-                    // mobile
-                    document.getElementById("mobile-toggle-theme").innerText = "· Dark"
-                }
-                window.localStorage &&
-                window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light',)
-            })
-        };
-        _Blog.toggleTheme();
-
-        // ready function.
-
+        lastScrollTop = st;
     }
-);
+
+    $('.article.gallery img:not(".not-gallery-item")').each(function () {
+        // wrap images with link and add caption if possible
+        if ($(this).parent('a').length === 0) {
+            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            if (this.alt) {
+                $(this).after('<div class="caption">' + this.alt + '</div>');
+            }
+        }
+    });
+
+    $('.article-entry').find('h1, h2, h3, h4, h5, h6').on('click', function () {
+        if ($(this).get(0).id) {
+            window.location.hash = $(this).get(0).id;
+        }
+    });
+
+    if (typeof(moment) === 'function') {
+        $('.article-meta time').each(function () {
+            $(this).text(moment($(this).attr('datetime')).fromNow());
+        });
+    }
+})(jQuery);
